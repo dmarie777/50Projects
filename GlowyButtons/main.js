350dc188bcb3aae644f63cc4da0b4dd014a95c3c
dmarie777 <deimarie777@gmail.com>
2024-02-04 18:09:59 -0500

    Added functionality to create a grid

---
+++ GlowyButtons/main.js
@@ -0,0 +1,18 @@
+const root = document.querySelector(':root');
+const container = document.querySelector('.container');
+const rootStyles = getComputedStyle(root)
+const windowHeight = window.innerHeight;
+const windowWidth = window.innerWidth;
+
+const gridColumns = rootStyles.getPropertyValue('--size');
+const gridWidth = Math.round(windowWidth/gridColumns);
+const gridRows = Math.round(windowHeight/gridWidth);
+
+function createGrid() {
+    for (let i=0; i<gridRows*gridColumns; i++) {
+        const newDiv = document.createElement('div');
+        newDiv.classList.add('grid');
+        container.appendChild(newDiv);
+    }
+}
+createGrid()
