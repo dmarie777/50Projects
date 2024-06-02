e0f65feb46517f0adda5efc55c68487774f01e95
dmarie777 <deimarie777@gmail.com>
2024-06-01 19:19:02 -0500

    Added functionality to draw the background and the boat

--- ColorsInCanvas/main.js
+++ ColorsInCanvas/main.js
@@ -11,10 +11,29 @@ window.addEventListener('resize', function() {
     canvas.height = window.innerHeight;
 })
 
-for (let i=10; i< window.innerWidth; i+=70) {
-    ctx.fillStyle = "rgb(200 0 0)";
-    ctx.fillRect(i, 10, 50, 50);
-    ctx.fillStyle = "rgb(0 0 200 / 50%)";
-    ctx.fillRect(i+30, 30, 50, 50);
+
+const arrColors = ["rgb(202, 240,248)", "rgb(173, 232,244)", "rgb(144, 224,239)", "rgb(72, 202,228)", "rgb(0, 180,216)", "rgb(0, 150,199)", "rgb(0, 119,182)", "rgb(2, 62,138)"]
+
+//background
+const height = window.innerHeight/arrColors.length;
+
+for (let y=0; y<arrColors.length; y++ ) {
+    ctx.fillStyle = arrColors[y];
+    ctx.fillRect(0, y*height, window.innerWidth, height );
 }
+
+//boat
+const x = window.innerWidth/3;
+const y = window.innerHeight/2;
+const radius = 200; // Arc radius
+const startAngle = Math.PI/6; // Starting point on circle
+const endAngle = Math.PI; 
+
+ctx.strokeStyle = "rgb(255, 255, 255)";
+ctx.fillStyle = "rgb(200, 230, 255)";
+ctx.arc(x, y, radius , startAngle, endAngle  )
+ctx.stroke();
+ctx.fill();
+ctx.fillStyle = "rgb(0, 119,182)";
+ctx.fillRect(x-150,y+170, 500, 50)
 
