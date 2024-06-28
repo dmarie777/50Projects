fc790b0d2482c2732742edfcbf9943fc98f24c22
dmarie777 <deimarie777@gmail.com>
2024-06-27 23:08:02 -0500

    Added a background and a color palette

--- Shapes/main.js
+++ Shapes/main.js
@@ -1,32 +1,43 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const scrollContainer = document.querySelector('.scroll-container');
-let scroll = 0;
+
+const colors = ['247 37 133', '181 23 158', '114 9 183', '86 11 173', '72 12 168', '58 12 163'];
+const back_color = 'rgb(13, 25, 57)';
+
+//Define canvas size
+canvas.width = scrollContainer.offsetWidth;
+canvas.height = scrollContainer.offsetHeight;
+
 
 scrollContainer.addEventListener('wheel', (event) => {
-    scroll = scroll + event.deltaY
-    console.log(scroll );
-    ctx.clearRect(0,0, canvas.width, canvas.height)
-    drawShapes();
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+    
+    //Draw background 
+    ctx.fillStyle = back_color;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    drawShapes();
 })
 
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-ctx.strokeStyle = 'red';
 
 let size=50;
 
 function drawShapes() {
-    for (let x = 0; x<window.innerWidth; x+=size) {
-        for(let y=0; y<window.innerHeight; y+=size) {
+    for (let x = 0; x<canvas.width; x+=size) {
+        for(let y=0; y<canvas.height-150; y+=size) {
             let options = Math.floor(Math.random()*3);
             if (options == 0) {
-                ctx.strokeRect(x,y, Math.random()*size, 200);
+                ctx.strokeStyle = `rgb(${colors[Math.floor(Math.random()*colors.length)]})`;
+                ctx.strokeRect(x,y, Math.random()*size, 150);
             } 
         } 
     }
 }
+//Draw background 
+ctx.fillStyle = back_color;
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 drawShapes();
 
