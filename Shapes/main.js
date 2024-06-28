4b96bcaa8a287b9a047001cc0b4c9f68e5f4c5ad
dmarie777 <deimarie777@gmail.com>
2024-06-28 09:05:38 -0500

    Modified the color values

--- Shapes/main.js
+++ Shapes/main.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const scrollContainer = document.querySelector('.scroll-container');
 
-const colors = ['247 37 133', '181 23 158', '114 9 183', '86 11 173', '72 12 168', '58 12 163'];
-const back_color = 'rgb(13, 25, 57)';
+const colors = ['144 202 249', '100 181 246', '66 165 245', '33 150 243', '30 136 229', '25 118 210', '21 101 192', '13 71 161']
+const back_color = 'rgb(5, 25, 35)';
 
 //Define canvas size
 canvas.width = scrollContainer.offsetWidth;
@@ -20,14 +20,12 @@ scrollContainer.addEventListener('wheel', (event) => {
     drawShapes();
 })
 
-
-
 let size=50;
 
 function drawShapes() {
     for (let x = 0; x<canvas.width; x+=size) {
         for(let y=0; y<canvas.height-150; y+=size) {
-            let options = Math.floor(Math.random()*3);
+            let options = Math.floor(Math.random()*4);
             if (options == 0) {
                 ctx.strokeStyle = `rgb(${colors[Math.floor(Math.random()*colors.length)]})`;
                 ctx.strokeRect(x,y, Math.random()*size, 150);
