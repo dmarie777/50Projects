00daaa93d96e73fd60f0f1941856dcc241d5bc06
dmarie777 <deimarie777@gmail.com>
2024-06-28 23:40:16 -0500

    Added new constructor

--- Shapes/main.js
+++ Shapes/main.js
@@ -5,6 +5,7 @@ const scrollContainer = document.querySelector('.scroll-container');
 const colors = ['144 202 249', '100 181 246', '66 165 245', '33 150 243', '30 136 229', '25 118 210', '21 101 192', '13 71 161']
 const back_color = 'rgb(5, 25, 35)';
 
+
 //Define canvas size
 canvas.width = scrollContainer.offsetWidth;
 canvas.height = scrollContainer.offsetHeight;
@@ -12,39 +13,56 @@ canvas.height = scrollContainer.offsetHeight;
 
 scrollContainer.addEventListener('wheel', (event) => {
     ctx.clearRect(0,0, canvas.width, canvas.height);
-    
-    drawBackground();
-    drawShapes();
+    drawer.drawBackground();
+    drawer.draw('triangle');
+
 })
 
 window.addEventListener('resize', () => {
     canvas.width = scrollContainer.offsetWidth;
     canvas.height = scrollContainer.offsetHeight;
-    drawBackground();
-    drawShapes();
+    drawer.drawBackground();
+    drawer.draw('triangle');
 })
 
-let size=50;
 
-function drawShapes() {
-    for (let x = 0; x<canvas.width; x+=size) {
-        for(let y=0; y<canvas.height-150; y+=size) {
-            let options = Math.floor(Math.random()*4);
-            if (options == 0) {
+class Drawer {
+    constructor(size) {
+        this.size = size
+    }
+    draw(shape) {
+        for (let x = 0; x<canvas.width; x+=this.size) {
+            for(let y=0; y<canvas.height-150; y+=this.size) {
+                let options = Math.floor(Math.random()*4);
                 ctx.strokeStyle = `rgb(${colors[Math.floor(Math.random()*colors.length)]})`;
-                ctx.strokeRect(x,y, Math.random()*size, 150);
+                if (options == 0 && shape==='rectangle') {
+                    ctx.strokeRect(x,y, Math.random()*this.size, 150);
+                } else if (options == 0 && shape==='circle') {
+                    ctx.beginPath();
+                    ctx.arc(x,y, size *2, 0, 2*Math.PI);
+                    ctx.stroke();
+                } else if (options == 0 && shape==='triangle'){
+                    ctx.beginPath();
+                            ctx.moveTo(x, y);
+                            ctx.lineTo(x + this.size / 2, y + this.size*2);
+                            ctx.lineTo(x - this.size / 2, y + this.size*3);
+                            ctx.closePath();
+                            ctx.stroke();
+
+                }
             } 
-        } 
+        }
+    }
+    drawBackground() {
+        ctx.fillStyle = back_color;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
     }
 }
 
-function drawBackground() {
-    ctx.fillStyle = back_color;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
+const drawer = new Drawer(50);
 
 window.onload = function() {
-    drawBackground();
-    drawShapes();
+    drawer.drawBackground();
+    drawer.draw('triangle');
 }
 
