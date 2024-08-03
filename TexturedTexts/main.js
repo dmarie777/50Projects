467d6d5ea86ced00b6e409d4cd8e0143812e61bc
dmarie777 <deimarie777@gmail.com>
2024-08-03 13:19:18 -0500

    Added texured text using the HTML5 canvas element

---
+++ TexturedTexts/main.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext("2d");
+
+const img = document.createElement("img");
+img.src = "img/reflective1.jpg";
+
+//FONTS
+//create the font
+const font = new FontFace("Panchang-Variable",  "url('./fonts/Panchang-Variable.woff')");
+////Load the font
+font.load().then(loadedFont => {
+    //Add the font to the document
+    document.fonts.add(loadedFont)
+    ctx.font = "35px Panchang-Variable"
+    if (img.complete) {
+        drawText();
+    } else {
+        img.onload = function() {
+            drawText();
+        }
+    }
+});
+
+function drawText() {
+    ctx.fillStyle = ctx.createPattern(img, 'repeat');
+    ctx.textAlign = 'start';
+    const x = canvas.width / 2;
+    ctx.fillText("USING THE HTML5 CANVAS", 0, 200);
+    ctx.fill();
+}
