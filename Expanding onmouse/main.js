565e8027ac7bcf48b1a566dfbf39ed26cecb64b8
dmarie777 <deimarie777@gmail.com>
2024-11-24 22:52:06 -0500

    Add lerp function to create angles

--- Expanding onmouse/main.js
+++ Expanding onmouse/main.js
@@ -1,7 +1,28 @@
 const panels = document.querySelectorAll('.panel')
+const navLinks = document.querySelectorAll('.nav__link')
+const rootStyles = getComputedStyle(document.documentElement)
+//convert steps to an odd number
+const steps = navLinks.length % 2 !==0? navLinks.length : navLinks.length + 1;
+//Create an array with numbers from 0 to PI
+const start = 0;
+const end = Math.PI;
+const angles = Array.from({length: steps}, (_,i) => lerp(start, end, i / steps));
 
+
+const radius = rootStyles.getPropertyValue('--radius')
+
+//
+navLinks.forEach(link => {
+    link.onmouseover = function (event) {
+        let target = event.target;
+        const idTarget = Number(target.id);
+        removeActive();
+        target.classList.add('active');
+    }
+})
+
+//Add the active class to the hover panels
 panels.forEach(panel => {
-
     panel.onmouseover = function (event) {
         let target = event.target;
         if (target.tagName === 'DIV') {
@@ -11,10 +32,13 @@ panels.forEach(panel => {
     }
 })
 
-
 function removeActive() {
      panels.forEach(panel => {
         panel.classList.remove('active')
      })
+}
+
+function lerp(a, b, t) {
+    return a + t * (b-a);
 }
 
