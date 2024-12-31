bed049c189870689a6681ae6e6b8048027f20111
dmarie777 <deimarie777@gmail.com>
2024-12-30 20:19:38 -0500

    Improve synchronization

--- GlowyButtons/main.js
+++ GlowyButtons/main.js
@@ -4,26 +4,23 @@ const rootStyles = getComputedStyle(root)
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
 
-const gridColumns = rootStyles.getPropertyValue('--size');
+const gridColumns = rootStyles.getPropertyValue('--col');
 const gridWidth = Math.round(windowWidth/gridColumns);
 const gridRows = Math.ceil(windowHeight/gridWidth);
 
 function createGrid() {
     for (let i=0; i<gridRows*gridColumns; i++) {
+
         const newDiv = document.createElement('div');
         newDiv.classList.add('grid');
         container.appendChild(newDiv);
     }
 }
-function setPosition() {
-    document.documentElement.style.setProperty('--width', `${windowWidth.toFixed(2)}` );
-}
 
 function syncPosition({x: pointerX, y: pointerY}) {
-    document.documentElement.style.setProperty('--x', pointerX);
-    document.documentElement.style.setProperty('--y', pointerY);
-    console.log(pointerX, pointerY);
+    document.documentElement.style.setProperty('--x', pointerX.toFixed(2));
+    document.documentElement.style.setProperty('--y', pointerY.toFixed(2));
 }
 document.addEventListener('pointermove', syncPosition)
-window.addEventListener("load", setPosition)
+
 createGrid()
