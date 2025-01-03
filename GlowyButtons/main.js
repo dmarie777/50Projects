23f21f94b37858bb8a8bb20d3bdf4b22c6f98b51
dmarie777 <deimarie777@gmail.com>
2025-01-03 07:41:40 -0500

    Add dat-gui library

--- GlowyButtons/main.js
+++ GlowyButtons/main.js
@@ -1,6 +1,6 @@
 const root = document.querySelector(':root');
 const container = document.querySelector('.container');
-const rootStyles = getComputedStyle(root)
+const rootStyles = getComputedStyle(root);
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
 
@@ -24,3 +24,37 @@ function syncPosition({x: pointerX, y: pointerY}) {
 document.addEventListener('pointermove', syncPosition)
 
 createGrid()
+const gridArr = document.querySelectorAll('.grid')
+
+//dat.GUI instance 
+const gui = new dat.GUI(); 
+const params = { 
+    rotationX: 0, 
+    rotationY: 0, 
+    rotationZ: 0, 
+    darkBack: `linear-gradient(rgb(27, 27, 27),rgb(27, 27, 27) ) no-repeat padding-box`,
+    lightBack: 'linear-gradient(rgb(245, 247, 250),rgb(240, 243, 248) ) no-repeat padding-box',
+    selectedBackground:`linear-gradient(rgb(27, 27, 27),rgb(27, 27, 27) ) no-repeat padding-box`,
+
+};
+
+document.body.style.background = params.darkBack;
+gridArr.forEach(grid => {
+    grid.style.background = params.darkBack;
+})
+
+gui.add(params, 'rotationX', -180, 180);
+gui.add(params, 'selectedBackground',['lightBack', 'darkBack']).name('Background Style').onChange(function(value) {
+    gridArr.forEach(grid => {
+        grid.style.background = params[value];
+    })
+    document.body.style.background = params[value];
+});
+
+function updateGrids() { 
+    console.log(`X: ${params.rotationX}, Y: ${params.rotationY}, Z: ${params.rotationZ}`);
+};
+
+
+gui.onChange(updateGrids);
+updateGrids();
