00a3d85eef40a8929bdc15b654967c9101fc9e9e
dmarie777 <deimarie777@gmail.com>
2025-01-04 22:14:30 -0500

    Add Grid class to initialize rows and columns

--- GlowyButtons/main.js
+++ GlowyButtons/main.js
@@ -4,57 +4,75 @@ const rootStyles = getComputedStyle(root);
 const windowHeight = window.innerHeight;
 const windowWidth = window.innerWidth;
 
-const gridColumns = rootStyles.getPropertyValue('--col');
-const gridWidth = Math.round(windowWidth/gridColumns);
-const gridRows = Math.ceil(windowHeight/gridWidth);
+//dat.GUI instance 
+const gui = new dat.GUI(); 
+const params = { 
+    numCol: 0, 
+    height: '10px', 
+    darkBack: `linear-gradient(rgb(27, 27, 27),rgb(27, 27, 27) ) no-repeat padding-box`,
+    lightBack: 'linear-gradient(rgb(245, 247, 250),rgb(245, 247, 250) ) no-repeat padding-box',
+    selectedBackground:`linear-gradient(rgb(27, 27, 27),rgb(27, 27, 27) ) no-repeat padding-box`,
+};
 
-function createGrid() {
-    for (let i=0; i<gridRows*gridColumns; i++) {
+class Grid {
+    constructor() {
+        this.numColumns = rootStyles.getPropertyValue('--col'),
+        this.gridHeight = rootStyles.getPropertyValue('--height').match(/\d+/)[0],
+        this.gridWidth  = Math.round(windowWidth/this.numColumns),
+        this.numRows = Math.ceil(windowHeight/this.gridHeight),
+        this.gridArr = [];
+    }
 
-        const newDiv = document.createElement('div');
-        newDiv.classList.add('grid');
-        container.appendChild(newDiv);
+    createGrid() {
+        for (let i=0; i<this.numRows*this.numColumns; i++) {
+            const newDiv = document.createElement('div');
+            newDiv.classList.add('grid');
+            container.appendChild(newDiv);
+            
+        }
+        this.gridArr = document.querySelectorAll('.grid');
+    }
+
+    updateGrid(val) {
+        this.gridArr.forEach(el => el.remove());
+        this.numColumns = val,
+        this.createGrid();
+    }
+
+    addBackground() {
+        document.body.style.background = params.selectedBackground;
+        
+        this.gridArr.forEach(cell => {
+            cell.style.background = params.selectedBackground;
+        })
     }
 }
 
+const grid = new Grid();
+grid.createGrid();
+grid.addBackground();
+
+
+
+//Light effect
 function syncPosition({x: pointerX, y: pointerY}) {
     document.documentElement.style.setProperty('--x', pointerX.toFixed(2));
     document.documentElement.style.setProperty('--y', pointerY.toFixed(2));
 }
-document.addEventListener('pointermove', syncPosition)
+document.addEventListener('pointermove', syncPosition);
 
-createGrid()
-const gridArr = document.querySelectorAll('.grid')
+//Dat-gui options
+gui.add(params, 'numCol', 1, 50).step(1).onChange(function(val) {
+    root.style.setProperty('--col', val);
+    grid.updateGrid(val);
+    grid.addBackground();
+});
 
-//dat.GUI instance 
-const gui = new dat.GUI(); 
-const params = { 
-    rotationX: 0, 
-    rotationY: 0, 
-    rotationZ: 0, 
-    darkBack: `linear-gradient(rgb(27, 27, 27),rgb(27, 27, 27) ) no-repeat padding-box`,
-    lightBack: 'linear-gradient(rgb(245, 247, 250),rgb(240, 243, 248) ) no-repeat padding-box',
-    selectedBackground:`linear-gradient(rgb(27, 27, 27),rgb(27, 27, 27) ) no-repeat padding-box`,
-
-};
-
-document.body.style.background = params.darkBack;
-gridArr.forEach(grid => {
-    grid.style.background = params.darkBack;
-})
-
-gui.add(params, 'rotationX', -180, 180);
 gui.add(params, 'selectedBackground',['lightBack', 'darkBack']).name('Background Style').onChange(function(value) {
-    gridArr.forEach(grid => {
+    params.selectedBackground = params[value];   
+    grid.gridArr.forEach(grid => {
         grid.style.background = params[value];
     })
     document.body.style.background = params[value];
 });
 
-function updateGrids() { 
-    console.log(`X: ${params.rotationX}, Y: ${params.rotationY}, Z: ${params.rotationZ}`);
-};
-
-
-gui.onChange(updateGrids);
-updateGrids();
